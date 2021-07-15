e8e85d41d956cfdbb664615954e290ec6076c623
zhang12138aaa <2942231368@qq.com>
2021-07-15 15:12:16 +0800

    test

--- src/main/java/com/springboot/travel/controller/TestController.java
+++ src/main/java/com/springboot/travel/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello world!  ";
     }
 
 }
