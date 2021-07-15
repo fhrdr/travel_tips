476c7fd3bf0d3756e52e0e3f961826fd79decf8a
zhang12138aaa <2942231368@qq.com>
2021-07-15 15:08:34 +0800

    test

---
+++ src/main/java/com/springboot/travel/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.springboot.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+//@Controller
+//@ResponseBody
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+
+}
